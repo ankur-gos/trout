852c2f0b326c9df85567e9d871a078424cfac04a
Ankur Goswami <agoswam3@ucsc.edu>
2016-11-18 21:22:00 -0800

    Add symtable.h

---
+++ symtable.h
@@ -0,0 +1,32 @@
+/*
+ * symtable.h
+ * symbol table struct and functions
+ * Ankur Goswami (agoswam3@ucsc.edu)
+ * Morgan Grant (mlgrant@ucsc.edu)
+ */
+
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "astree.h"
+
+enum { ATTR_void, ATTR_int, ATTR_null, ATTR_string,
+       ATTR_struct, ATTR_array, ATTR_function, ATTR_variable,
+       ATTR_field, ATTR_typeid, ATTR_param, ATTR_lval, ATTR_const,
+       ATTR_vreg, ATTR_vaddr, ATTR_bitset_size };
+
+// bitset
+using attr_bitset = bitset<ATTR_bitset_size>;
+
+struct symbol;
+using symbol_table = unordered_map<const string*, symbol*>;
+
+struct symbol{
+    attr_bitset attributes;
+    symbol_table* fields;
+    location lloc;
+    size_t block_nr;
+    vector<symbol*> parameters;
+}
