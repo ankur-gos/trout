fab8a8e07a60ec77f97eb736d4841a9d8d7516f4
Ankur Goswami <agoswam3@ucsc.edu>
2016-11-21 19:50:21 -0800

    dump ast after symbol table

--- oc.cpp
+++ oc.cpp
@@ -143,8 +143,8 @@ int main(int argc, char** argv){
 
    scan_tok_to_file (no_ext, command);
    dump_stringset_file (no_ext);
-   dump_ast(no_ext);
    create_symbol_table(no_ext);
+   dump_ast(no_ext);
    check_types(parser::root);
    if(pclose(yyin) != 0){
       cerr << "Pipe close failed." << endl;
